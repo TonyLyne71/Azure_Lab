9b848ea65418bd9f82463ad60a5e2d3e5a030d22
Tony Lyne <tonylyne71@gmail.com>
2024-07-30 15:10:26 +1200

    Update Providers.tf

--- Providers.tf
+++ Providers.tf
@@ -1,10 +1,6 @@
 terraform {
   required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~>3.0"
-    }
-    random = {
+        random = {
       source  = "hashicorp/random"
       version = "~>3.0"
     }
