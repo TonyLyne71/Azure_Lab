177c43125da168b63caea5b00a93ea3cdedce7ce
Tony Lyne <tonylyne71@gmail.com>
2024-07-31 16:35:19 +1200

    Update Providers.tf

--- Providers.tf
+++ Providers.tf
@@ -11,4 +11,8 @@ provider "azurerm" {
   skip_provider_registration = true
   # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
+  subscription_id = '29d195e6-5fcc-4cac-b030-95e71dac3cb7'
+  client_id       = 'tf@tonylyne71gmail.onmicrosoft.com'
+  client_secret   = 'Spr1ng1953^!'
+  tenant_id       = '98edf4b6-0d36-49ce-8c7f-aa7d799bf343'
 }
