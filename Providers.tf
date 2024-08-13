1ab86974553d2e47d3afa53ca38f87c22e277a63
Tony Lyne <tonylyne71@gmail.com>
2024-08-13 14:47:36 +1200

    Update Providers.tf

--- Providers.tf
+++ Providers.tf
@@ -2,17 +2,29 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=3.0.0"
+      version = "~>3.0"
+    }
+    github = {
+      source  = "integrations/github"
+      version = "~> 6.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~>3.0"
     }
   }
-}
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate13517"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+  }
 
+}
 provider "azurerm" {
-  skip_provider_registration = true
-  # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
-  subscription_id = '29d195e6-5fcc-4cac-b030-95e71dac3cb7'
-  client_id       = 'tf@tonylyne71gmail.onmicrosoft.com'
-  client_secret   = 'Spr1ng1953^!'
-  tenant_id       = '98edf4b6-0d36-49ce-8c7f-aa7d799bf343'
+}
+provider "github" {
+  token = "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"
+  owner = "Tony Lyne"
 }
